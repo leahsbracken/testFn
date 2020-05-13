d3d2416c78f235ddfaa2329eb236fcd264e9c620
Leah Bracken <lbracken17@gmail.com>
2020-05-13 14:13:41 -0400

    "Please work"

--- func.py
+++ func.py
@@ -3,7 +3,7 @@ import json
 
 from fdk import response
 
-directors_dict = ({"Psycho": "Alfred Hitchcock", "Batman": "Tim Burton", "Alien": "Ridley Scott", "Predator": "John McTiernan", "Mission Impossible": "John Woo"})
+directors_dict = ({"Psycho": "Alfred Hitchcock", "Batman": "Tim Burton", "Alien": "Ridley Scott", "Cats": "Tom Hooper"})
 
 def handler(ctx, data: io.BytesIO=None):
     movie = "Psycho"
